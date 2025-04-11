2959deee61a43ac2420787b38b70f9e18eed966a
MuhammadUmar7831 <mu8494759@gmail.com>
2025-04-11 18:53:11 +0500

    data not used build issue

--- src/components/home/Gallery.tsx
+++ src/components/home/Gallery.tsx
@@ -18,7 +18,6 @@ export type GallerySlide = {
 };
 
 export default async function Gallery() {
-  const data = await getSimpleText();
   const slides: GallerySlide[] = await getGalleryItems();
 
   return (
